542e4f737861209c71741d28e3fbd1eac4135509
Amrit <jaiswal.amrit@gmail.com>
2019-09-01 20:37:08 +0530

    test

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -8,7 +8,10 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class);
-		
 	}
-
+	
+	public void test() {
+		System.out.println();
+	}
+	
 }
